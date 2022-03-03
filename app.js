f57c6f5ed4f857753821b161e85e5873e6e0bcf9
Farid ullah <faridullah996@gmail.com>
2022-03-03 16:09:51 +0500

    added morgan

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const studentsRouter = require('./routes/studentsRoute');
 const employeeRouter = require('./routes/employeeRoute');
 const courseRouter = require('./routes/courseRoute');
 
+app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+	console.log('Morgan enabled.');
+	app.use(morgan('tiny'));
+}
+
 app.get('/', (req, res) => {
 	res.status(200).send('Welcome to admin portal')
 });
-
-app.use(express.json());
 
 app.use('/api/v1/students', studentsRouter);
 app.use('/api/v1/employees', employeeRouter);
