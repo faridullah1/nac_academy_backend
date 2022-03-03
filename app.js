20c72202dd56d86acd894667457e0967485f4a94
Farid ullah <faridullah996@gmail.com>
2022-03-03 17:20:48 +0500

    preparing app for deployment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const compression = require('compression');
 const app = express();
 
 const studentsRouter = require('./routes/studentsRoute');
@@ -8,6 +9,7 @@ const courseRouter = require('./routes/courseRoute');
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use(compression());
 
 if (process.env.NODE_ENV === 'development') {
 	console.log('Morgan enabled.');
