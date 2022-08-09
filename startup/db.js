a979173ae9811e7fcd52db57f445d0ec16967b5d
Farid ullah <faridullah996@gmail.com>
2022-08-09 13:02:58 +0500

    created separate env variable for DATABASE_PASSWORD

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-	let databaseURL = process.env.DATABASE;
+	let db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 	if (process.env.NODE_ENV === 'development') {
-		databaseURL = process.env.DATABASE_LOCAL;
+		db = process.env.DATABASE_LOCAL;
 	}
 
-	mongoose.connect(databaseURL)
+	mongoose.connect(db)
 		.then(() => console.log('Connected To Database'))
 		.catch(error => console.log('error connecting to db:', error));
 }
