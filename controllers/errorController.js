16e058f422feb15e1a3765aac2455d65e3e29e62
Farid ullah <faridullah996@gmail.com>
2022-08-10 15:36:34 +0500

    error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,64 @@
+const AppError = require("../utils/appError");
+
+handleCastErrorDB = err => {
+	const message = `Invalid ${err.path}: ${err.value}`;
+	return new AppError(message, 400)
+}
+
+handleDuplicateFieldDB = err => {
+	const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+	const message = `Invalid field value: ${value}. Please use another one.`;
+	return new AppError(message, 400)
+}
+
+handleValidationErrorDB = err => {
+	const errors = Object.values(err.errors).map(el => el.message);
+	const message = `Invalid input data. ${errors.join(', ')}`;
+	return new AppError(message, 400)
+}
+
+sendErrorDev = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		error: err,
+		message: err.message,
+		stack: err.stack
+	});
+}
+
+sendErrorProd = (err, res) => {
+	// Operational, trusted errors: send error message to the client.
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message,
+		});	
+	}
+	// Programming or other unknown errors: don't leak error details.
+	else {
+		// 1) Log error
+		console.error('Programming or unknown error =>', err);
+
+		// 2) Send generic message
+		res.status(500).json({
+			status: 'error',
+			message: 'Something went wrong',
+		});	
+	}
+}
+
 module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || 'error';
 
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message
-	});
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(err, res);
+	}
+	else if (process.env.NODE_ENV === 'production') {
+		if (err.name === 'CastError') err = handleCastErrorDB(err);
+		else if (err.code  === 11000) err = handleDuplicateFieldDB(err);
+		else if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
+
+		sendErrorProd(err, res);
+	}
 }
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', err => {
+	console.log('Uncaught Exception, shutting down.');
+	console.log(err.name, err.message);
+	process.exit(1);
+});
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+	console.log('Unhandled Rejection, shutting down.');
+	console.log(err.name, err.message);
+	server.close(() => process.exit(1));
 });
