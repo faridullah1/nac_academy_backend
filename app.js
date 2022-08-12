84b347028edda22ab90e779ba5c0913b367c61bc
Farid ullah <faridullah996@gmail.com>
2022-08-12 14:45:51 +0500

    added helmet package to add security headers for response.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const cors = require('cors');
 const compression = require('compression');
+const helmet = require('helmet');
 const express = require('express');
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
+app.use(helmet());
 
 require('./startup/config')(app);
 require('./startup/db')();
